823bec3de826da636b0034338ef0f799b8f918f4
dmitri <deemok@gmail.com>
2020-02-21 22:00:00 +0100

    Forward-port SELinux changes from 6.1.x

--- tool/gravity/cli/rpcagent.go
+++ tool/gravity/cli/rpcagent.go
@@ -201,6 +201,7 @@ func rpcAgentDeployHelper(ctx context.Context, localEnv *localenv.LocalEnvironme
 		proxy:        proxy,
 		leaderParams: leaderParams,
 		nodeParams:   nodeParams,
+		seLinux:      cluster.SELinux,
 	}
 
 	// Force this node to be the operation leader
@@ -319,6 +320,7 @@ func newDeployAgentsRequest(ctx context.Context, req deployAgentsRequest) (*rpc.
 		LeaderParams:   req.leaderParams,
 		Leader:         req.leader,
 		NodeParams:     req.nodeParams,
+		SELinux:        req.seLinux,
 	}, nil
 }
 
@@ -402,4 +404,5 @@ type deployAgentsRequest struct {
 	leaderParams string
 	leader       *storage.Server
 	nodeParams   string
+	seLinux      bool
 }
